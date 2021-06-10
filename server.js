886f0b44927cc9efe91de18c08dce2b95e433476
jack <jack.connor83@gmail.com>
2021-06-09 17:08:24 -0700

    added server and procfile

---
+++ server.js
@@ -0,0 +1,22 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    // if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    //     return res.redirect('https://' + req.get('host') + req.url);
+    // }
+    next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/iraqWar'));
+console.log('APP SET')
+app.get('/*', function(req, res) {
+  console.log('GOT ONE')
+  console.log(req)
+  res.sendFile('index.html', {root: './dist/iraqWar/'});
+});
+
+app.listen(5555, () => {
+  console.log('Listening on 5555');
+});
